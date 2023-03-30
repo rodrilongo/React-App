7e4ab3e889ad1e85680630904b91022622df2866
Rodrigo Longo <rodrigolongo2002@gmail.com>
2023-03-30 19:34:50 +0200

    Primer commit

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./itemlistcontainer.css"
+
+export const ItemListContainer = (props) => {
+  return (
+    <div>
+      <h3>{props.greeting}</h3>
+    </div>
+  )
+}
+
+
